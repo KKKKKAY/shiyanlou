ddca2db2e04838a2af70579ca4fbada86c7c3870
KKKKKAY <601618053@qq.com>
2018-12-20 16:01:58 +0800

    update1220

--- calculator.py
+++ calculator.py
@@ -3,14 +3,25 @@
 
 import sys
 
-if(len(sys.argv)==2):
-    salary=sys.argv[1]
-    # print(salary)
-    try:
-        salaryInt=int(salary)
+def insured_count(salary):
+    insured=""
+    old=0.08
+    hos=0.02
+    job=0.005
+    hurt=0
+    born=0
+    home=0.06
+    insured=salary*(old+hos+job+hurt+born+home)
+    
+    return format(insured,'.2f')
+
+
+def tax_count(salary,insured):
+    
         tax=""
         baseSalary=3500
-        gapSalary=salaryInt-baseSalary
+        
+        gapSalary=salary-insured-baseSalary
         if(gapSalary>0 and gapSalary<=1500):
             tax=gapSalary*0.03
         elif(gapSalary>1500 and gapSalary<=4500):
@@ -27,11 +38,33 @@ if(len(sys.argv)==2):
             tax=gapSalary*0.45-13505
         else:
             tax=0
-        print(format(tax,".2f"))
-    except ValueError:
-        ("Parameter Error")
-    
+        return format(tax,".2f")
+
+
+if __name__=='__main__':
+    if(len(sys.argv)>=2):
+        output_item={}
+        for input_item in sys.argv[1:]:
+            item=input_item.split(':')
+            name=item[0]
+            try:
+                salary=int(item[1])
+            except ValueError:
+                print("Parameter Error")
+            
+            insured_fee=float(insured_count(salary))       
+            tax_fee=float(tax_count(salary,insured_fee))
+            salary_after_tax=salary-insured_fee-tax_fee
+            output_item[name]=format(salary_after_tax,'.2f')
         
-else:
-    print("Parameter Error")
-    
+        
+        
+        for key,value in output_item.items():
+            print("{}:{}".format(key,value))
+    else:
+        print("Parameter Error")
+
+
+
+
+
