2f5bd76996e7b9fbab6bb22ade53d8b1b569eb63
KKKKKAY <601618053@qq.com>
2018-12-28 13:18:07 +0800

    update1881228

--- news/app.py
+++ news/app.py
@@ -8,15 +8,25 @@ app=Flask(__name__)
 
 @app.route('/')
 def index():
+    filename=[]
+    directory=os.path.join(os.getcwd(),"..")
+    json_file=[f for f in os.listdir(directory) if ".json" in f]
+    for s in json_file:
+        filepath=os.path.join(os.getcwd(),"..",s)
+        
+        with open(filepath) as f:
+            data=json.load(f)
+            filename.append(data.get("title"))
+        
+    
 
-
-    return render_template("index.html",filename
+    return render_template("index.html",filename=filename)
 
 
 @app.route('/files/<filename>')
 def file(filename):
 
-
+    pass
 
 
 if __name__=="__main__":
